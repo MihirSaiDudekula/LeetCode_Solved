fac5d3300051351b0ec31986bd6695960bbcaa17
MihirSaiDudekula <mihirsaidudekula@gmail.com>
2023-10-08 07:44:29 +0530

    changes in folder structure
    quick and easy to understand

--- Arrays/EASY/Build Array from Permutation/Answer.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def buildArray(self, nums: List[int]) -> List[int]:
-        l = len(nums)
-        a=[]
-        for x in range(0,l):
-            a.append(nums[nums[x]])
-        return a
